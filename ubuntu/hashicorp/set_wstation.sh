7afac9c0ffa4b8f70bde6e151a8c33066b449d35
Jesus Maria Garcia <sales@msquaredtc.com>
2020-12-13 12:21:01 -0800

    MicroK8s

--- ubuntu/hashicorp/set_wstation.sh
+++ ubuntu/hashicorp/set_wstation.sh
@@ -18,8 +18,22 @@ function install_hashi () {
             printf "===== INSTALLING LATEST ${package} =====\n"
             sudo apt -qq install ${package} -y
         done
-        
+
+}
+
+function install_microk8s () {
+
+    printf "===== INSTALLING MICROK8S KUBERNETES =====\n"
+    snap install microk8s --classic
+
+    printf "===== ENABLING BASIC SERVICES =====\n"
+    microk8s enable dashboard dns registry
+
+    printf "===== STOPPING KUBERNETES =====\n"
+    microk8s stop
+
 }
 
 # main
 install_hashi
+install_microk8s
